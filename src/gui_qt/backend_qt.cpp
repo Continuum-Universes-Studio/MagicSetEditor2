ad9905f8c0e870dfb8b0feada2f2dd92079d9f01
Christian Daniel Jensen <cdjensen94@gmail.com>
2026-02-09 17:54:35 -0700

    Fallback to wx welcome from Qt backend

--- src/gui_qt/backend_qt.cpp
+++ src/gui_qt/backend_qt.cpp
@@ -14,11 +14,13 @@
 #include <data/set.hpp>
 #include <data/settings.hpp>
 #include <gui_qt/windows.hpp>
+#include <gui_wx/app_wx.hpp>
 #include <util/prec.hpp>
 #include <util/io/package_manager.hpp>
 #include <util/spell_checker.hpp>
 #include <wx/filename.h>
 #include <wx/filefn.h>
+#include <wx/init.h>
 
 #include <QApplication>
 #include <QDateTime>
@@ -134,9 +136,18 @@ class QtBackend final : public Backend {
 public:
   int run(int argc, char** argv) override {
     log_line("Starting Qt backend.");
+    if (argc <= 1 || (argc == 2 && std::string(argv[1]) == "--color")) {
+      log_line("No startup arguments provided; handing off to wxWidgets welcome window.");
+      return run_wx_app(argc, argv);
+    }
     qInstallMessageHandler(qt_message_handler);
     QApplication app(argc, argv);
     log_line("QApplication created.");
+    wxInitializer wx_init;
+    if (!wx_init) {
+      log_line("Failed to initialize wxWidgets.");
+      return EXIT_FAILURE;
+    }
 
     init_script_variables();
     init_file_formats();
